039fff8ec5b6b678d0d40b98b1cbe98f678a51af
Igor Spitkovskyi <Igor.Spitkovskyi@microfocus.com>
2023-02-20 19:02:49 +0200

    initial commit

---
+++ collections/Tupel.py
@@ -0,0 +1,8 @@
+#Tuple has differences from Lists
+#Tuple is IMMUTABLE (once created, it cannot be changed)
+coordinates = (4, 5)
+list_of_tuples = [(4, 5), (6, 7), (80, 34)]
+print(coordinates[0], coordinates[1])
+
+#INVALID OPERATION
+#   coordinates[0] = 3
---
+++ functions/func1.py
@@ -0,0 +1,11 @@
+
+def say_hi_user():
+    print("Hello User")
+def sayhi(username, age):
+    print("Hello", username + ",", "you are", age)
+
+print("Top")
+say_hi_user()
+sayhi("Igor", 46)
+sayhi("Kate", 43)
+print("Bottom")
