71eeff959c2a6a42a4f5d1f582b7461ca1381610
Igor Spitkovskyi <Igor.Spitkovskyi@microfocus.com>
2023-03-12 17:23:49 +0200

    two dim array updates

--- collections/two_dimension_lists.py
+++ collections/two_dimension_lists.py
@@ -5,3 +5,12 @@ number_grid = [
     [7, 8, 9],
     [0]
 ]
+
+print("Print element #2 in line #1:", number_grid[0][1])
+print("Print element #2 in line #3:", number_grid[2][1])
+
+print("####### Go through all elements of each array ################")
+for row in number_grid:
+    for col in row:
+        print(col)
+    print("")
